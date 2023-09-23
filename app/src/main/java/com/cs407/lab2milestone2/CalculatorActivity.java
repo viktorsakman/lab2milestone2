09691cbdb893e6663ae23787be3c9f8e3c6d8ea7
viktorsakman <viktorsakman@gmail.com>
2023-09-23 14:56:37 -0500

    Initial commit

---
+++ app/src/main/java/com/cs407/lab2milestone2/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package com.cs407.lab2milestone2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+    }
+}
